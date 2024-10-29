6c9d24c8ac75ef8a35cc4ab3805df5f1b4c36000
Emile <emile.saintgirons@student-cs.fr>
2024-10-29 13:43:20 +0100

    bis

--- Q4.py
+++ Q4.py
@@ -1,6 +1,6 @@
 from Monte_Carlo import *
 		
-def Question_4(T, nb_pas_MC, B_min, B_max, n):
+def Question_4(nb_pas_MC, B_min, B_max, n):
 	energies = []
 	moments = []
 	capacites = []
@@ -9,36 +9,23 @@ def Question_4(T, nb_pas_MC, B_min, B_max, n):
 	
 	for B in champs:
 		print('B =',B)
-		E, C, M = Monte_Carlo(T,nb_pas_MC,B, n)
+		E, C, M = Monte_Carlo(1,nb_pas_MC,B, n)
 		energies.append(E)
 		moments.append(M)
 		capacites.append(C)
 
-	fig, ax = plt.subplots(3,1, sharex=True, figsize=(10,9))
+	fig, ax = plt.subplots(1,1, sharex=True, figsize=(10,3))
 	fig.tight_layout(h_pad=2, w_pad=2)
     
 
-    # Tracé de l'énergie
-	ax[0].plot(champs, energies)
-	ax[0].set_ylabel('$E$')
- #   ax[0].set_yscale('log')
-	ax[0].grid(visible=True, which="both")
-
-    # Tracé du moment magnétique
-	ax[1].plot(champs, moments, color='green')
-	ax[1].set_ylabel(r'$\mu$')
-	ax[1].grid(visible=True, which="both")
-#    ax[1].set_yscale('log')
-
-	# Tracé de la capacité thermique
-	ax[2].plot(champs, capacites, color='orange')
-	ax[2].set_ylabel('$c_v$')
-	ax[2].set_xlabel('B')
-	ax[2].grid(visible=True, which="both")
-	ax[2].set_yscale('log')
+	ax.plot(champs, capacites, color='orange')
+	ax.set_ylabel('$c_v$')
+	ax.set_xlabel('B')
+	ax.grid(visible=True, which="both")
+	ax.set_yscale('log')
 
 	plt.tight_layout()
-	plt.savefig('Figures/Q3_nb_pas_MC='+str(nb_pas_MC)+'_T='+str(T)+'_n='+str(n)+'.pdf', bbox_inches="tight")
+	plt.savefig('Figures/Q4_nb_pas_MC='+str(nb_pas_MC)+'_Bmin='+str(B_min)+'_Bmax='+str(B_max)+'_n='+str(n)+'.pdf', bbox_inches="tight")
 	plt.close()
 	
-Question_4(T=1, nb_pas_MC=50000, B_min=0.1, B_max=5, n=10)
+Question_4(nb_pas_MC=50000, B_min=0.1, B_max=5, n=10)
